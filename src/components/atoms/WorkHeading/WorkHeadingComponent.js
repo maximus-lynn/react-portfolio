dc4b608ecfcd918a80b26f9eb82a60e5bcbef6ac
Max Lynn <max.lynn@shiftactivemedia.com>
2018-03-17 18:08:44 +0000

    adding heading to the work page. On to the singles

---
+++ src/components/atoms/WorkHeading/WorkHeadingComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WorkHeading.scss';
+
+const WorkHeading = () => {
+    return (
+        <h1 className="work__heading">
+            Work
+        </h1>
+    );
+}
+
+export default WorkHeading;
--- src/components/pages/WorkComponent.js
+++ src/components/pages/WorkComponent.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import PortfolioItem from '../molecules/portfolio-item/PortfolioItemComponent';
+import WorkHeading from '../atoms/WorkHeading/WorkHeadingComponent';
 
 const Work = () => {
     return (
         <React.Fragment>
+            <WorkHeading></WorkHeading>
+            <PortfolioItem></PortfolioItem>
             <PortfolioItem></PortfolioItem>
         </React.Fragment>
     );
