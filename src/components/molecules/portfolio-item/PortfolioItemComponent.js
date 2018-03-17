5c4bc2fd29b1b4648c87202e61504b6303b34f69
Max Lynn <max.lynn@shiftactivemedia.com>
2018-03-17 14:21:35 +0000

    adding portfolio section and making it responsive

--- src/components/molecules/portfolio-item/PortfolioItemComponent.js
+++ src/components/molecules/portfolio-item/PortfolioItemComponent.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import './Portfolio-item.scss';
 
+import yt from './imgs/yt.jpg';
+import macgregor from './imgs/macgreg.jpg';
+import controltech from './imgs/controltech.jpg';
+
 const PortfolioItem = () => {
     return (
         <section className="portfolio">
             <article className="portfolio__item">
-                <img src="http://via.placeholder.com/400x535" className="portfolio__image" alt=""/>
+                <img src={yt} className="portfolio__image" alt=""/>
                 <header className="portfolio__content">
                     <div className="portfolio__heading">
-                        <h1 class="portfolio__title">YOUNG TALENT</h1>
-                        <p className="portfolio__subtitle">YT INDUSTRIES</p>
+                        <h1 className="portfolio__title">YOUNG TALENT</h1>
+                        <h6 className="portfolio__subtitle">YT INDUSTRIES</h6>
                     </div>
                 </header>
             </article>
             <article className="portfolio__item">
-                <img src="http://via.placeholder.com/400x535" className="portfolio__image" alt=""/>
+                <img src={macgregor} className="portfolio__image" alt=""/>
                 <header className="portfolio__content">
                     <div className="portfolio__heading">
-                        <h1 class="portfolio__title">client one</h1>
-                        <p className="portfolio__subtitle">Description</p>
+                        <h1 className="portfolio__title">MACGREGOR SMITH</h1>
+                        <h6 className="portfolio__subtitle">LANDSCAPE ARCHITECTURE</h6>
                     </div>
                 </header>
             </article>
             <article className="portfolio__item">
-                <img src="http://via.placeholder.com/400x535" className="portfolio__image" alt=""/>
+                <img src={controltech} className="portfolio__image" alt=""/>
                 <header className="portfolio__content">
                     <div className="portfolio__heading">
-                        <h1 class="portfolio__title">client one</h1>
-                        <p className="portfolio__subtitle">Description</p>
+                        <h1 className="portfolio__title">CONTROLTECH</h1>
+                        <h6 className="portfolio__subtitle">BIKE PRODUCERS</h6>
                     </div>
                 </header>
             </article>                        
